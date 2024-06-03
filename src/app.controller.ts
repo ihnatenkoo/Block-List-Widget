5f936a165b7de7db5ef0920a9d2b4f0f290f1767
Dmytro Ihnatenko <ihnatenko.dev@gmail.com>
2024-06-03 10:19:07 +0300

    Connect to prisma client

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,22 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { PrismaClient } from '@prisma/client';
+import { ApiProperty } from '@nestjs/swagger';
+const prisma = new PrismaClient();
+
+class HelloWorldDto {
+  @ApiProperty()
+  message: string;
+}
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  async getHello(): Promise<HelloWorldDto> {
+    const users = await prisma.user.findMany({});
+    console.log('✎ - users:', users);
+    return { message: this.appService.getHello() };
   }
 }
