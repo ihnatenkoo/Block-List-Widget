6ac399386c976d3245658ee167c5853f381efd69
Dmytro Ihnatenko <ihnatenko.dev@gmail.com>
2024-06-04 12:42:11 +0300

    Implement sign-up and sign-in

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,61 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Post,
+  Res,
+} from '@nestjs/common';
+import { ApiCreatedResponse, ApiOkResponse } from '@nestjs/swagger';
+import { Response } from 'express';
+import { GetSessionInfoDto, SignInBodyDto, SignUpBodyDto } from './dto';
+import { AuthService } from './auth.service';
+import { CookieService } from './cookie.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(
+    private authService: AuthService,
+    private cookieService: CookieService,
+  ) {}
+
+  @Post('sign-up')
+  @ApiCreatedResponse()
+  async signUp(
+    @Body() body: SignUpBodyDto,
+    @Res({ passthrough: true }) res: Response,
+  ): Promise<void> {
+    const { accessToken } = await this.authService.signUp(
+      body.email,
+      body.password,
+    );
+
+    this.cookieService.setToken(res, accessToken);
+  }
+
+  @Post('sign-in')
+  @ApiOkResponse()
+  @HttpCode(HttpStatus.OK)
+  async signIn(
+    @Body() body: SignInBodyDto,
+    @Res({ passthrough: true }) res: Response,
+  ): Promise<void> {
+    const { accessToken } = await this.authService.signIn(
+      body.email,
+      body.password,
+    );
+    this.cookieService.setToken(res, accessToken);
+  }
+
+  @Post('sign-out')
+  @ApiOkResponse()
+  @HttpCode(HttpStatus.OK)
+  signOut() {}
+
+  @Get('session')
+  @ApiOkResponse({
+    type: GetSessionInfoDto,
+  })
+  getSessionInfo() {}
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { DbService } from 'src/db/db.service';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(private bdService: DbService) {}
+
+  async findByEmail(email: string) {
+    return this.bdService.user.findFirst({ where: { email } });
+  }
+
+  async create(email: string, hash: string, salt: string) {
+    return this.bdService.user.create({ data: { email, hash, salt } });
+  }
+}
